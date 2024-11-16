a63a919caaf7a3508733331a8c7d100b15252fcc
caining <channingliu1115@gmail.com>
2024-11-16 18:46:27 +0800

    [blog]feat: add system design

---
+++ leetcode/LRU.py
@@ -0,0 +1,59 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+    
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+    
+    def _move_to_head(self, node):
+        self._remove(node)
+        self._add_to_head(node)
+    
+    def _remove(self, node):
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+    
+    def _add_to_head(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+    
+    def _remove_tail(self):
+        node = self.tail.prev
+        self._remove(node)
+        return node
+    
+    def get(self, key):
+        if key in self.cache:
+            node = self.cache[key]
+            self._move_to_head(node)
+            return node.value
+        return -1
+
+    def put(self, key, value):
+        if key in self.cache:
+            node = self.cache[key]
+            node.value = value
+            self._move_to_head(node)
+        else:
+            new_node = Node(key, value)
+            self.cache[key] = new_node
+            self._add_to_head(new_node)
+            if len(self.cache) > self.capacity:
+                tail_node = self._remove_tail()
+                del self.cache[tail_node.key]
+    
+    
+    
